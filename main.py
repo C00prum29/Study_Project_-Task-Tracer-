cd712e799f0f23f20d3c39218f74d0d1704d68fe
c00prum <gavrish.mail@gmail.com>
2025-12-21 23:15:53 +0300

    deleted:    data.json 	modified:   main.py 	new file:   tasks.json

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import typer, json
+from datetime import datetime
 from pathlib import Path
 
 folder = Path("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-")
-file_path = folder / "data.json"
+file_path = folder / "tasks.json"
 
 folder.mkdir(parents=True, exist_ok=True)
 
@@ -12,8 +13,28 @@ if not file_path.exists():
 app = typer.Typer()
 
 @app.command()
-def hello():
-    print("Hello")
+def add(description: str, status: str):
+    with open("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-/tasks.json", "r", encoding="utf-8") as file:
+        tasks = json.load(file)
 
+        task_id = str(len(tasks) + 1)
+
+        tasks[task_id] = {
+        "Task ID": task_id,
+        "description": description,
+        "status": status,   
+        "created_at": datetime.now().isoformat()
+        }
+
+    with open("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-/tasks.json", "w", encoding="utf-8") as file:
+        json.dump(tasks, file, ensure_ascii=False, indent=5)
+    print(f"Task {description} added successfully.")
+
+@app.command()
+def goodbye():
+    print("Goodbye!")
+    
 if __name__ == "__main__":
     app()
+
+
