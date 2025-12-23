962c3d5f906ed02b276b6a64d3c834d805a067ac
c00prum <gavrish.mail@gmail.com>
2025-12-23 17:00:58 +0300

    modified:   main.py 	modified:   tasks.json

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if not file_path.exists():
 app = typer.Typer()
 
 @app.command()
-def add(description: str, status: str):
+def add(description: str):
     with open("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-/tasks.json", "r", encoding="utf-8") as file:
         tasks = json.load(file)
 
@@ -22,8 +22,9 @@ def add(description: str, status: str):
         tasks[task_id] = {
         "Task ID": task_id,
         "description": description,
-        "status": status,   
-        "created_at": datetime.now().isoformat()
+        "status": "to do",   
+        "created_at": datetime.now().isoformat(),
+        "updated_at": datetime.now().isoformat()
         }
 
     with open("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-/tasks.json", "w", encoding="utf-8") as file:
@@ -31,8 +32,44 @@ def add(description: str, status: str):
     print(f"Task {description} added successfully.")
 
 @app.command()
-def goodbye():
-    print("Goodbye!")
+def delete(task_id: str):
+    with open("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-/tasks.json", "r", encoding="utf-8") as file:
+        tasks = json.load(file)
+        if task_id in tasks:
+            del tasks[task_id]
+            with open("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-/tasks.json","w", encoding="utf-8") as file:
+                json.dump(tasks, file, ensure_ascii=False, indent=5)
+                print(f"Task {task_id} deleted successfully")
+        else:
+            print(f"Task {task_id} not found.")
+
+@app.command()
+def update(task_id: str, description: str):
+    with open("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-/tasks.json", "r", encoding="utf-8") as file:
+        tasks = json.load(file)
+        if task_id in tasks:
+            tasks[task_id]["description"] = description
+            tasks[task_id]["updated_at"] = datetime.now().isoformat()
+            with open("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-/tasks.json","w", encoding="utf-8") as file:
+                json.dump(tasks, file, ensure_ascii=False, indent=5)
+                print(f"Task {task_id} updated successfully")
+        else:
+            print(f"Task {task_id} not found")
+
+@app.command()
+def mark_done(task_id: str):
+    with open("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-/tasks.json", "r", encoding="utf-8") as file:
+        tasks = json.load(file)
+        if task_id in tasks:
+            tasks[task_id]["status"] = "done"
+            tasks[task_id]["updated_at"] = datetime.now().isoformat()
+            with open("C:/Users/cprum/Desktop/Прога/Task_tracker_project/Study_Project_-Task-Tracer-/tasks.json","w", encoding="utf-8") as file:
+                json.dump(tasks, file, ensure_ascii=False, indent=5)
+                print(f"Task {task_id} marked as done successfully")
+        else:
+            print(f"Task {task_id} not found")
+
+
     
 if __name__ == "__main__":
     app()
